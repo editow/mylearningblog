e601dd5d51465e655fd1d050afcc86d36a1d2b66
myProjects <chenzhitao70@gmail.com>
2018-06-26 23:36:58 +0800

    Ready for deployment to heroku.

---
+++ Procfile
@@ -0,0 +1,2 @@
+web:gunicorn learningBlog.wsgi --log-file -
+
---
+++ learning_log/admin.py
@@ -0,0 +1,9 @@
+# coding:utf8
+from django.contrib import admin
+from learning_log.models import Topic,Entry
+
+# Register your models here.
+admin.site.register(Topic)
+admin.site.register(Entry)
+
+
